59d86b8e61cc953ff208c029602b4e942765dcb7
zMokhtari <zahramokhtari97@gmail.com>
2025-04-17 02:11:49 +0330

    feat: implement task form modal with out data

--- src/app/shared/components/task-card/task-card.component.ts
+++ src/app/shared/components/task-card/task-card.component.ts
@@ -1,9 +1,11 @@
-import { Component, input, model, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskModalComponent } from '../task-modal/task-modal.component';
 
 @Component({
   selector: 'app-task-card',
@@ -25,4 +27,18 @@ export class TaskCardComponent {
   onMoveToDaily = output<boolean>();
 
   task = input({});
+
+  readonly dialog = inject(MatDialog);
+
+  editTaskDialog() {
+    const dialogRef = this.dialog.open(TaskModalComponent, {
+      data: {
+        isEditMode: true,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
--- src/app/shared/components/task-modal/task-modal.component.ts
+++ src/app/shared/components/task-modal/task-modal.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-task-modal',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './task-modal.component.html',
-  styleUrl: './task-modal.component.css'
+  styleUrl: './task-modal.component.css',
 })
 export class TaskModalComponent {
+  isEditMode = false;
+  readonly dialog = inject(MatDialog);
+  readonly data = inject<any>(MAT_DIALOG_DATA);
 
+  constructor() {
+    this.isEditMode = this.data?.isEditMode ?? false;
+  }
+
+  taskForm = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    description: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.taskForm.value);
+  }
 }
