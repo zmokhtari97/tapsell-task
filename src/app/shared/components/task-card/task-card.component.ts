9071a611bd495e70af8078cfbef471145b7e8d35
zMokhtari <zahramokhtari97@gmail.com>
2025-04-17 01:30:41 +0330

    feat: implement task card design with out data

--- src/app/shared/components/task-card/task-card.component.ts
+++ src/app/shared/components/task-card/task-card.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, input, model, output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-card',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCheckboxModule,
+    FormsModule,
+  ],
   templateUrl: './task-card.component.html',
-  styleUrl: './task-card.component.css'
+  styleUrl: './task-card.component.css',
 })
 export class TaskCardComponent {
+  editable = input(true);
+  ableToCheck = input(true);
+  ableToMoveDaily = input(true);
 
+  onMoveToDaily = output<boolean>();
+
+  task = input({});
 }
