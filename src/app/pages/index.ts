ee22821ea09c74dbb526689d392227be201d34af
zMokhtari <zahramokhtari97@gmail.com>
2025-04-16 12:45:30 +0330

    refactor: move components to shared

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -2,4 +2,3 @@ export * from './main-task-list/main-task-list.component';
 export * from './daily-task-list/daily-task-list.component';
 export * from './single-task-list/single-task-list.component';
 export * from './completed-task-list/completed-task-list.component';
-export * from './components/index';
