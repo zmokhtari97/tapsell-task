04b4e62eda0a76370153c417cbef46c865352586
zMokhtari <zahramokhtari97@gmail.com>
2025-04-17 14:34:42 +0330

    feat: add app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import {
+  CompletedTaskListComponent,
+  DailyTaskListComponent,
+  IntroductionComponent,
+  MainTaskListComponent,
+  SingleTaskListComponent,
+} from './pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'introduction',
+    component: IntroductionComponent,
+  },
+  {
+    path: 'main',
+    component: MainTaskListComponent,
+  },
+  {
+    path: 'complete',
+    component: CompletedTaskListComponent,
+  },
+  {
+    path: 'list',
+    component: DailyTaskListComponent,
+  },
+  {
+    path: 'list/:id',
+    component: SingleTaskListComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'main',
+  },
+];
--- src/app/layout/sidebar-layout/sidebar-layout.component.ts
+++ src/app/layout/sidebar-layout/sidebar-layout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-layout',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidebar-layout.component.html',
   styleUrl: './sidebar-layout.component.scss',
 })
