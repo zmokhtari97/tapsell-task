d1e76ff192daef16749c969f5e75eb4163ef3d2e
zMokhtari <zahramokhtari97@gmail.com>
2025-04-17 16:41:55 +0330

    feat: create single-task-list component to display the data

--- src/app/pages/single-task-list/single-task-list.component.ts
+++ src/app/pages/single-task-list/single-task-list.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { List, ListService, TaskListComponent } from '../../shared';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-task-list',
-  imports: [],
+  imports: [TaskListComponent],
   templateUrl: './single-task-list.component.html',
-  styleUrl: './single-task-list.component.css'
+  styleUrl: './single-task-list.component.css',
 })
 export class SingleTaskListComponent {
+  private readonly listService = inject(ListService);
+  list: List | undefined;
+  listId: string | null = null;
+  activatedRoute = inject(ActivatedRoute);
 
+  ngOnInit(): void {
+    this.listId = this.activatedRoute.snapshot.paramMap.get('id');
+    if (this.listId) {
+      this.getListData();
+    }
+  }
+
+  getListData() {
+    this.listService.getListById(this.listId!).subscribe((data) => {
+      this.list = data;
+    });
+  }
 }
