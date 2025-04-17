be80276cc11145ce0e9200cda977e12f79638b3f
zMokhtari <zahramokhtari97@gmail.com>
2025-04-17 16:40:58 +0330

    feat: create a simple for of ui for all lists

--- src/app/pages/all-task-list/all-task-list.component.ts
+++ src/app/pages/all-task-list/all-task-list.component.ts
@@ -1,20 +1,45 @@
 import { Component, inject } from '@angular/core';
-import { List, ListService } from '../../shared';
+import { List, ListModalComponent, ListService } from '../../shared';
+import { RouterModule } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-all-task-list',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './all-task-list.component.html',
   styleUrl: './all-task-list.component.scss',
 })
 export class AllTaskListComponent {
   listService = inject(ListService);
-
   allList: List[] = [];
+
+  readonly dialog = inject(MatDialog);
+
   ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
     this.listService.getAllLists().subscribe((data) => {
       this.allList = data;
       console.log(this.allList);
+    });
+  }
+
+  openAddDialog() {
+    const dialogRef = this.dialog.open(ListModalComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(result);
+      this.addList(result);
+    });
+  }
+
+  addList(title: string) {
+    let model = {
+      title: title,
+    };
+    this.listService.createList(model).subscribe(() => {
+      this.getData();
     });
   }
 }
